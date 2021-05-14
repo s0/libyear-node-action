70afd3068f8cb94531d6aa8b8f0ce360392ee16d
Sam Lanning <sam@samlanning.com>
2021-05-14 09:39:33 +0100

    🔧 Increase timeout of unit tests

--- test/jest-global-setup-hooks.ts
+++ test/jest-global-setup-hooks.ts
@@ -6,3 +6,9 @@ const ROOT = path.dirname(__dirname);
 const LIBYEAR = path.join(ROOT, 'node_modules/libyear/bin/libyear');
 
 setLibyearPath(LIBYEAR);
+
+/**
+ * Given the nature of running libyear (i.e. network requests) tests can take
+ * a minute or so
+ */
+jest.setTimeout(1000 * 60);
