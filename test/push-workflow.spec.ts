5bc1e8c9e9feaddbc96737c83e9bc4702fcb7d49
Sam Lanning <sam@samlanning.com>
2021-05-14 11:21:01 +0100

    ✅ Add unit test to support FOLDER argument

--- test/push-workflow.spec.ts
+++ test/push-workflow.spec.ts
@@ -9,7 +9,48 @@ describe('push-workflow', () => {
       eventJson: {},
       eventName: 'push',
       logToConsole: false,
-      copySpec: '001',
+      copySpec: {
+        spec: '001',
+      },
+    });
+
+    const spec = await loadSpec('001');
+    const specTotals = getTotals(spec);
+
+    // Ensure that table is printed out
+    expect(run.stdout).toMatch(
+      `TABLE: ${JSON.stringify(getResultsTable(spec))}`
+    );
+
+    // Ensure that variables are set
+    for (const metric of ['drift', 'pulse'] as const) {
+      expect(specTotals.get(metric)).toBeTruthy();
+      expect(run.stdout).toMatch(
+        `::set-output name=${metric}::${Number(specTotals.get(metric)).toFixed(
+          2
+        )}`
+      );
+    }
+    for (const metric of ['releases', 'major', 'minor', 'patch'] as const) {
+      expect(specTotals.get(metric)).toBeTruthy();
+      expect(run.stdout).toMatch(
+        `::set-output name=${metric}::${specTotals.get(metric)}`
+      );
+    }
+  });
+
+  it('sub-directory-config', async () => {
+    const run = await runAction({
+      eventJson: {},
+      eventName: 'push',
+      logToConsole: false,
+      copySpec: {
+        spec: '001',
+        subdir: ['some', 'deep', 'directory'],
+      },
+      extraEnv: {
+        FOLDER: 'some/deep/directory',
+      },
     });
 
     const spec = await loadSpec('001');
